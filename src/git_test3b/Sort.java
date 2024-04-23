7837ca737a4817834dbd49564badf5a444391be1
Choi, Sungho <gfhyrfgjh@gmail.com>
2024-04-23 16:36:13 +0900

    문법 오류 재수정

--- src/git_test3b/Sort.java
+++ src/git_test3b/Sort.java
@@ -15,15 +15,19 @@ public class Sort {
  static int partition(int[] a, int start, int end) {
     int value = a[end];
     int i = start - 1;
-    for (int j = start; j <= end - 1; ++j)
-        if (a[j] < value)
-            swap(a, ++i, j);
+    for (int j = start; j <= end - 1; ++j) {
+		if (a[j] < value) {
+			swap(a, ++i, j);
+		}
+	}
     swap(a, i + 1, end);
     return i + 1;
  }
 
  static void quickSort(int[] a, int start, int end) {
-     if (start >= end) return;
+     if (start >= end) {
+		return;
+	}
         int middle = partition(a, start, end);
     quickSort(a, start, middle-1);
     quickSort(a, middle+1, end);
